93dc6b1606feddf2eca89fc7cf78927be16facfe
Michael Hum <michaelhum@cmail.carleton.ca>
2015-10-06 21:40:29 -0400

    Added shell player.

---
+++ src/main/java/ca/carleton/blackjack/Player.java
@@ -0,0 +1,9 @@
+package ca.carleton.blackjack;
+
+/**
+ * Represents a player.
+ *
+ * Created by Mike on 10/6/2015.
+ */
+public class Player {
+}
--- src/main/java/ca/carleton/blackjack/controller/EchoWebSocketHandler.java
+++ src/main/java/ca/carleton/blackjack/controller/EchoWebSocketHandler.java
@@ -1,5 +1,6 @@
 package ca.carleton.blackjack.controller;
 
+import ca.carleton.blackjack.Player;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,14 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Web socket for simple echos.
- *
+ * <p/>
  * Created by Mike on 10/6/2015.
  */
 @Component
@@ -19,14 +25,36 @@ public class EchoWebSocketHandler extends TextWebSocketHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(EchoWebSocketHandler.class);
 
+    private static final int MAX_PLAYERS = 4;
+
+    private Map<WebSocketSession, Player> players;
+
     private int count = 1;
 
     @Autowired
     private EchoService echoService;
 
+    @PostConstruct
+    public void init() {
+        players = new HashMap<>();
+    }
+
     @Override
     public void afterConnectionEstablished(final WebSocketSession session) {
-        LOG.info("Opened new session for {}.", session);
+        LOG.info("Opened new session for {}.", session.getId());
+        players.putIfAbsent(session, new Player());
+        LOG.info("Added player for session {}.", session.getId());
+
+        // Tell others of a connection
+        players.keySet().stream()
+                .filter(val -> !session.getId().equals(val.getId()))
+                .forEach(val -> {
+                    try {
+                        val.sendMessage(new TextMessage(String.format("Client %s has connected!", session.getId())));
+                    } catch (final IOException exception) {
+                        LOG.error("Unable to send message.", exception);
+                    }
+                });
     }
 
     @Override
